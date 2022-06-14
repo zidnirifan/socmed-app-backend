c3e0d540b5b5232d9f2063621f541f2ee4e4a6ab
zidni <zidni.rifan7@gmail.com>
2022-06-14 09:55:24 +0700

    add config.ts

---
+++ src/Commons/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  mongoHost: process.env.MONGO_HOST,
+  mongoPort: process.env.MONGO_PORT,
+  mongoDB: process.env.MONGO_DB,
+  testDB: process.env.MONGO_DB_TEST,
+};
+
+export default config;
