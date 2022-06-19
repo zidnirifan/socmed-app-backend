d0652315ef9997be6089d24f93c6ec3575cecc9d
zidni <zidni.rifan7@gmail.com>
2022-06-19 16:58:58 +0700

    add config.serverPort

--- src/Commons/config.ts
+++ src/Commons/config.ts
@@ -3,6 +3,7 @@ const config = {
   mongoPort: process.env.MONGO_PORT,
   mongoDB: process.env.MONGO_DB,
   testDB: process.env.MONGO_DB_TEST,
+  serverPort: process.env.SERVER_PORT,
 };
 
 export default config;
