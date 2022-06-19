6556fd83f008271877706fe26c31f2f28453e98b
zidni <zidni.rifan7@gmail.com>
2022-06-19 10:30:54 +0700

    delete verifyPayload method on User.ts

--- src/Domains/users/entities/test/User.test.ts
+++ src/Domains/users/entities/test/User.test.ts
@@ -1,68 +1,17 @@
-import User, { IUser } from '../User';
+import User from '../User';
 
 describe('a User entities', () => {
-  it('should throw error when payload not contain needed property', () => {
-    const payload = {
-      username: 'jhondoe',
-    };
-
-    expect(() => new User(payload as IUser)).toThrowError(
-      'USER.NOT_CONTAIN_NEEDED_PROPERTY'
-    );
-  });
-
-  it('should throw error when payload not meet data type spesification', () => {
-    const payload = {
-      username: 121334,
-      fullName: true,
-      password: {},
-    };
-
-    expect(() => new User(payload as unknown as IUser)).toThrowError(
-      'USER.NOT_MEET_DATA_TYPE_SPESIFICATION'
-    );
-  });
-
-  it('should throw error when username less than 5 character', () => {
-    const payload = {
-      username: 'jhon',
-      fullName: 'Jhon Doe',
-      password: 'password',
-    };
-
-    expect(() => new User(payload)).toThrowError('USER.USERNAME_MINIMAL_CHAR');
-  });
-
-  it('should throw error when username contains more than 50 character', () => {
-    const payload = {
-      username: 'wkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkwkw',
-      fullName: 'wadidaw',
-      password: 'password',
-    };
-
-    expect(() => new User(payload)).toThrowError('USER.USERNAME_LIMIT_CHAR');
-  });
-
-  it('should throw error when username contains restricted character', () => {
-    const payload = {
-      username: 'jhon doe',
-      fullName: 'Jhon Doe',
-      password: 'password',
-    };
-
-    expect(() => new User(payload)).toThrowError(
-      'USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER'
-    );
-  });
-
-  it('should throw error when password less than 8 character', () => {
+  it('should add blank string value to optional property when optional property not filled', () => {
     const payload = {
       username: 'jhondoe',
       fullName: 'Jhon Doe',
-      password: 'secret',
+      password: 'password',
     };
 
-    expect(() => new User(payload)).toThrowError('USER.PASSWORD_MINIMAL_CHAR');
+    const { profilePhoto, bio } = new User(payload);
+
+    expect(profilePhoto).toEqual('');
+    expect(bio).toEqual('');
   });
 
   it('should create user object correctly', () => {
