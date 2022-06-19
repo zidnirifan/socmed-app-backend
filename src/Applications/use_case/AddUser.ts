d4b91795531bb3f7b06c80c671503809240d1637
zidni <zidni.rifan7@gmail.com>
2022-06-19 10:56:15 +0700

    add validator on AddUser use case

--- src/Applications/use_case/AddUser.ts
+++ src/Applications/use_case/AddUser.ts
@@ -1,23 +1,28 @@
 import User, { IUser } from '../../Domains/users/entities/User';
 import { IUserRepository } from '../../Domains/users/UserRepository';
 import { IPasswordHash } from '../security/PasswordHash';
+import { IValidator } from '../validator/Validator';
 
-interface IDepedency {
+interface IDependency {
   userRepository: IUserRepository;
   passwordHash: IPasswordHash;
+  validator: IValidator<IUser>;
 }
 
 class AddUser {
   private userRepository: IUserRepository;
   private passwordHash: IPasswordHash;
+  private validator: IValidator<IUser>;
 
-  constructor(depedency: IDepedency) {
-    this.userRepository = depedency.userRepository;
-    this.passwordHash = depedency.passwordHash;
+  constructor(dependency: IDependency) {
+    this.userRepository = dependency.userRepository;
+    this.passwordHash = dependency.passwordHash;
+    this.validator = dependency.validator;
   }
 
-  async execute(useCasePayload: IUser) {
-    const user = new User(useCasePayload);
+  async execute(payload: IUser) {
+    this.validator.validate(payload);
+    const user = new User(payload);
     await this.userRepository.verifyAvailableUsername(user.username);
     user.password = await this.passwordHash.hash(user.password);
     return this.userRepository.addUser(user);
--- src/Applications/use_case/test/AddUser.test.ts
+++ src/Applications/use_case/test/AddUser.test.ts
@@ -1,6 +1,7 @@
 import User from '../../../Domains/users/entities/User';
 import MockUserRepository from '../../../Domains/users/test/UserRepositoryTestHelper';
 import MockPasswordHash from '../../security/test/PasswordHashTestHelper';
+import MockValidator from '../../validator/test/ValidatorTestHelper';
 import AddUser from '../AddUser';
 
 describe('AddUser use case', () => {
@@ -14,6 +15,7 @@ describe('AddUser use case', () => {
 
     const mockUserRepository = new MockUserRepository();
     const mockPasswordHash = new MockPasswordHash();
+    const mockValidator = new MockValidator();
 
     mockUserRepository.verifyAvailableUsername = jest.fn(() =>
       Promise.resolve()
@@ -22,15 +24,18 @@ describe('AddUser use case', () => {
     mockPasswordHash.hash = jest.fn(() =>
       Promise.resolve('encrypted_password')
     );
+    mockValidator.validate = jest.fn(() => {});
 
     const addUserUseCase = new AddUser({
       userRepository: mockUserRepository,
       passwordHash: mockPasswordHash,
+      validator: mockValidator,
     });
 
     const userId = await addUserUseCase.execute(useCasePayload);
 
     expect(userId).toEqual(id);
+    expect(mockValidator.validate).toBeCalledWith(useCasePayload);
     expect(mockUserRepository.verifyAvailableUsername).toBeCalledWith(
       useCasePayload.username
     );
