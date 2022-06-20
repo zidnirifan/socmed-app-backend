6c389919af1426b65eb83fa2f5947100bfd2de34
zidni <zidni.rifan7@gmail.com>
2022-06-20 13:48:45 +0700

    refactor Validator

--- src/Infrastructures/validator/Validator.ts
+++ src/Infrastructures/validator/Validator.ts
@@ -1,8 +1,10 @@
-import { ObjectSchema } from 'joi';
+import Joi, { ObjectSchema } from 'joi';
 import ValidatorAbstract from '../../Applications/validator/Validator';
 import InvariantError from '../../Commons/exceptions/InvariantError';
 
 abstract class Validator<Payload> extends ValidatorAbstract<Payload> {
+  protected Joi = Joi;
+
   validate(payload: Payload): void {
     const validationResult = this.schema().validate(payload);
     if (validationResult.error) {
