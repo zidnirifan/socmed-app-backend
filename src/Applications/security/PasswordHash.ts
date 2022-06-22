4a2b7f0649d656a27b993f1111d9880b4e2b1cdb
zidni <zidni.rifan7@gmail.com>
2022-06-22 13:23:20 +0700

    add function comparePassword

--- src/Applications/security/PasswordHash.ts
+++ src/Applications/security/PasswordHash.ts
@@ -4,6 +4,11 @@ export interface IPasswordHash {
 
 abstract class PasswordHash implements IPasswordHash {
   abstract hash(password: string): Promise<string>;
+
+  abstract comparePassword(
+    password: string,
+    encryptedPassword: string
+  ): Promise<void>;
 }
 
 export default PasswordHash;
