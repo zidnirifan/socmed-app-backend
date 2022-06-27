9c739071b7e723dd06766cae50f4992971b1d3d2
zidni <zidni.rifan7@gmail.com>
2022-06-27 16:55:00 +0700

    EditProfilePhoto use case

---
+++ src/Applications/use_case/EditProfilePhoto.ts
@@ -0,0 +1,41 @@
+import ProfilePhoto, {
+  IProfilePhoto,
+} from '../../Domains/users/entities/ProfilePhoto';
+import { IUserRepository } from '../../Domains/users/UserRepository';
+import { IImageResizer } from '../storage/ImageResizer';
+import { IStorage } from '../storage/Storage';
+import { IValidator } from '../validator/Validator';
+
+interface Dependency {
+  validator: IValidator<IProfilePhoto>;
+  imageResizer: IImageResizer;
+  storage: IStorage;
+  userRepository: IUserRepository;
+}
+
+class EditProfilePhoto {
+  validator: IValidator<IProfilePhoto>;
+  imageResizer: IImageResizer;
+  storage: IStorage;
+  userRepository: IUserRepository;
+
+  constructor(dependency: Dependency) {
+    this.validator = dependency.validator;
+    this.imageResizer = dependency.imageResizer;
+    this.storage = dependency.storage;
+    this.userRepository = dependency.userRepository;
+  }
+
+  async execute(payload: IProfilePhoto) {
+    this.validator.validate(payload);
+    await this.userRepository.isUserExistById(payload.userId);
+    const { path, fileName, userId } = new ProfilePhoto(payload);
+    const widthImg = 400;
+    const buffer = await this.imageResizer.resizeImageToBuffer(path, widthImg);
+    const photoUrl = this.storage.writeFileFromBuffer(buffer, fileName);
+    await this.userRepository.editProfilePhotoById(userId, photoUrl);
+    return photoUrl;
+  }
+}
+
+export default EditProfilePhoto;
---
+++ src/Applications/use_case/test/EditProfilePhoto.test.ts
@@ -0,0 +1,62 @@
+import EditProfilePhoto from '../EditProfilePhoto';
+import MockUserRepository from '../../../Domains/users/test/UserRepositoryTestHelper';
+import MockImageResizer from '../../storage/test/ImageResizerTestHelper';
+import MockStorage from '../../storage/test/StorageTestHelper';
+import MockValidator from '../../validator/test/ValidatorTestHelper';
+
+describe('EditProfilePhoto use case', () => {
+  it('should orchestrating the edit profile photo action correctly', async () => {
+    // Arrange
+    const payload = {
+      userId: 'user-123',
+      path: '/images/img.jpg',
+      fileName: 'img.jpg',
+      fileType: 'image/jpeg' as const,
+    };
+
+    const buffer = new Uint8ClampedArray([1, 2]);
+    const profilePhotoUrl = 'http://image.com/img.jpg';
+
+    const mockValidator = new MockValidator();
+    const mockUserRepository = new MockUserRepository();
+    const mockImageResizer = new MockImageResizer();
+    const mockStorage = new MockStorage();
+
+    // Mocking
+    mockValidator.validate = jest.fn(() => {});
+    mockUserRepository.isUserExistById = jest.fn(() => Promise.resolve());
+    mockImageResizer.resizeImageToBuffer = jest.fn(() =>
+      Promise.resolve(buffer)
+    );
+    mockStorage.writeFileFromBuffer = jest.fn(() => profilePhotoUrl);
+    mockUserRepository.editProfilePhotoById = jest.fn(() => Promise.resolve());
+
+    // Create use case instance
+    const editProfilePhoto = new EditProfilePhoto({
+      validator: mockValidator,
+      userRepository: mockUserRepository,
+      imageResizer: mockImageResizer,
+      storage: mockStorage,
+    });
+
+    // Action
+    const profilePhoto = await editProfilePhoto.execute(payload);
+
+    // Assert
+    expect(profilePhoto).toEqual(profilePhotoUrl);
+    expect(mockValidator.validate).toBeCalledWith(payload);
+    expect(mockUserRepository.isUserExistById).toBeCalledWith(payload.userId);
+    expect(mockImageResizer.resizeImageToBuffer).toBeCalledWith(
+      payload.path,
+      400
+    );
+    expect(mockStorage.writeFileFromBuffer).toBeCalledWith(
+      buffer,
+      payload.fileName
+    );
+    expect(mockUserRepository.editProfilePhotoById).toBeCalledWith(
+      payload.userId,
+      profilePhotoUrl
+    );
+  });
+});
