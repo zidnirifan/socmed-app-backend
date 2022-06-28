c5d59250e476fffd53cff609820a65a7a43261e2
zidni <zidni.rifan7@gmail.com>
2022-06-28 14:11:58 +0700

    edit ImageResizer test

--- src/Infrastructures/storage/test/ImageResizer.test.ts
+++ src/Infrastructures/storage/test/ImageResizer.test.ts
@@ -1,14 +1,16 @@
+import path from 'path';
 import ImageResizer from '../ImageResizer';
 
 describe('ImageResizer', () => {
   describe('resizeImageToBuffer function', () => {
     it('should resize image correctly and return buffer Uint8ClampedArray', async () => {
+      const imgPath = path.resolve(
+        __dirname,
+        '../../../test/images/gedang.jpg'
+      );
       const imageResizer = new ImageResizer();
 
-      const buffer = await imageResizer.resizeImageToBuffer(
-        'src/infrastructures/storage/test/images/gedang.jpg',
-        400
-      );
+      const buffer = await imageResizer.resizeImageToBuffer(imgPath, 400);
 
       expect(buffer).toBeInstanceOf(Uint8ClampedArray);
     });
