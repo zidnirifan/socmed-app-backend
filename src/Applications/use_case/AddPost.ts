8b8449c2eb60d02c2498707941cd1e0faf34f900
zidni <zidni.rifan7@gmail.com>
2022-06-29 14:04:45 +0700

    AddPost use case

---
+++ src/Applications/use_case/AddPost.ts
@@ -0,0 +1,54 @@
+import Post, { IPost } from '../../Domains/posts/entities/Post';
+import { IPostRepository } from '../../Domains/posts/PostRepository';
+import { IImageResizer } from '../storage/ImageResizer';
+import { IStorage } from '../storage/Storage';
+import { IValidator } from '../validator/Validator';
+
+interface Dependency {
+  validator: IValidator<IPost>;
+  postRepository: IPostRepository;
+  imageResizer: IImageResizer;
+  storage: IStorage;
+}
+
+class AddPost {
+  private validator: IValidator<IPost>;
+  private postRepository: IPostRepository;
+  private imageResizer: IImageResizer;
+  private storage: IStorage;
+
+  constructor(dependency: Dependency) {
+    this.validator = dependency.validator;
+    this.postRepository = dependency.postRepository;
+    this.imageResizer = dependency.imageResizer;
+    this.storage = dependency.storage;
+  }
+
+  async execute(payload: IPost): Promise<string> {
+    this.validator.validate(payload);
+    const post = new Post(payload);
+    const imgWidth = 400;
+
+    const buffers = await Promise.all(
+      post.media.map(async (e) => {
+        const buffer = await this.imageResizer.resizeImageToBuffer(
+          e.path,
+          imgWidth
+        );
+        return { buffer, fileName: e.fileName };
+      })
+    );
+
+    const fileNames = buffers.map((b) =>
+      this.storage.writeFileFromBuffer(b.buffer, b.fileName)
+    );
+
+    return this.postRepository.addPost({
+      userId: post.userId,
+      caption: post.caption,
+      media: fileNames,
+    });
+  }
+}
+
+export default AddPost;
---
+++ src/Applications/use_case/test/AddPost.test.ts
@@ -0,0 +1,64 @@
+import AddPost from '../AddPost';
+import MockPostRepository from '../../../Domains/posts/test/PostRepositoryTestHelper';
+import MockImageResizer from '../../storage/test/ImageResizerTestHelper';
+import MockStorage from '../../storage/test/StorageTestHelper';
+import MockValidator from '../../validator/test/ValidatorTestHelper';
+
+describe('AddPost use case', () => {
+  it('should orchestrating add post action correctly', async () => {
+    // Arrange
+    const payload = {
+      userId: 'user-123',
+      caption: 'helo ges',
+      media: [
+        {
+          path: '/images/img.jpg',
+          fileName: 'img.jpg',
+          fileType: 'image/jpeg' as const,
+        },
+      ],
+    };
+
+    const buffer = new Uint8ClampedArray([1, 2]);
+    const mediaUrl = 'http://image.com/img.jpg';
+    const postId = 'post-123';
+
+    const mockValidator = new MockValidator();
+    const mockPostRepository = new MockPostRepository();
+    const mockImageResizer = new MockImageResizer();
+    const mockStorage = new MockStorage();
+
+    // Mocking
+    mockValidator.validate = jest.fn(() => {});
+    mockImageResizer.resizeImageToBuffer = jest.fn(() =>
+      Promise.resolve(buffer)
+    );
+    mockStorage.writeFileFromBuffer = jest.fn(() => mediaUrl);
+    mockPostRepository.addPost = jest.fn(() => Promise.resolve(postId));
+
+    // Create use case instance
+    const addPost = new AddPost({
+      validator: mockValidator,
+      postRepository: mockPostRepository,
+      imageResizer: mockImageResizer,
+      storage: mockStorage,
+    });
+
+    // Action
+    const id = await addPost.execute(payload);
+
+    // Assert
+    expect(id).toEqual(postId);
+    expect(mockValidator.validate).toBeCalledWith(payload);
+    expect(mockImageResizer.resizeImageToBuffer).toBeCalledWith(
+      payload.media[0].path,
+      400
+    );
+    expect(mockStorage.writeFileFromBuffer).toBeCalled();
+    expect(mockPostRepository.addPost).toBeCalledWith({
+      userId: payload.userId,
+      caption: payload.caption,
+      media: [mediaUrl],
+    });
+  });
+});
