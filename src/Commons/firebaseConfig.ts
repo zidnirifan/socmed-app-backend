e1eab898a67622fe574bbd7b20433abbdf3fb581
zidni <zidni.rifan7@gmail.com>
2022-07-07 10:59:53 +0700

    firebase config

---
+++ src/Commons/firebaseConfig.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  appId: process.env.APP_ID,
+};
+
+export default firebaseConfig;
