a63b5f55e4991cb96014bb66d8216ea287e0ec28
zidni <zidni.rifan7@gmail.com>
2022-07-07 19:29:45 +0700

    edit testHelper.getToken

--- src/test/testHelper.ts
+++ src/test/testHelper.ts
@@ -14,18 +14,21 @@ const testHelper = {
       password: 'password',
     };
 
-    await supertest(app).post('/users').send(user);
+    const { body: bodyUser } = await supertest(app).post('/users').send(user);
 
     const { body } = await supertest(app)
       .post('/auth')
       .send({ username: user.username, password: user.password });
 
-    return body.data.accessToken;
+    return {
+      id: bodyUser.data.userId.toString(),
+      token: body.data.accessToken,
+    };
   },
   postPost: async () => {
     const mediaPath = path.resolve(__dirname, './images/gedang.jpg');
     const caption = 'helo ges';
-    const token = await testHelper.getToken();
+    const { token } = await testHelper.getToken();
 
     const { body } = await supertest(app)
       .post('/posts')
