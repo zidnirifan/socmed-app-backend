34e98cf3e54af79c2d85c0a62970a873f8ffc53e
zidni <zidni.rifan7@gmail.com>
2022-08-01 19:22:17 +0700

    Comment validator

---
+++ src/Infrastructures/validator/comment/CommentValidator.ts
@@ -0,0 +1,18 @@
+import { ObjectSchema } from 'joi';
+import { IComment } from '../../../Domains/comments/entities/Comment';
+import Validator from '../Validator';
+
+class CommentValidator extends Validator<IComment> {
+  protected schema(): ObjectSchema<IComment> {
+    const { Joi } = this;
+
+    return Joi.object({
+      userId: Joi.string().required(),
+      content: Joi.string().required(),
+      postId: Joi.string().required(),
+      replyTo: Joi.string(),
+    });
+  }
+}
+
+export default CommentValidator;
