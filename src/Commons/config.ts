620f556975a21eac458354a1076d5dea0188ed34
zidni <zidni.rifan7@gmail.com>
2022-08-17 22:57:49 +0700

    fix port

--- src/Commons/config.ts
+++ src/Commons/config.ts
@@ -3,7 +3,7 @@ const config = {
   mongoPort: process.env.MONGO_PORT,
   mongoDB: process.env.MONGO_DB,
   testDB: process.env.MONGO_DB_TEST,
-  serverPort: process.env.SERVER_PORT,
+  serverPort: process.env.PORT,
   accessTokenKey: process.env.ACCESS_TOKEN_KEY,
   refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
   tokenExpiration: parseInt(process.env.TOKEN_EXPIRATION as string, 10),
