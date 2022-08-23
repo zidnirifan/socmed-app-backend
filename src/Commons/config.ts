1015d54cc10cabb246ce36054a8b8f1ec0af9a35
zidni <zidni.rifan7@gmail.com>
2022-08-23 10:09:46 +0700

    fix db and test

--- src/Commons/config.ts
+++ src/Commons/config.ts
@@ -1,13 +1,11 @@
 const config = {
-  mongoHost: process.env.MONGO_HOST,
-  mongoPort: process.env.MONGO_PORT,
-  mongoDB: process.env.MONGO_DB,
-  testDB: process.env.MONGO_DB_TEST,
   serverPort: process.env.PORT,
   accessTokenKey: process.env.ACCESS_TOKEN_KEY,
   refreshTokenKey: process.env.REFRESH_TOKEN_KEY,
   tokenExpiration: parseInt(process.env.TOKEN_EXPIRATION as string, 10),
   mongoUrl: process.env.MONGO_URL as string,
+  mongoUrlDev: process.env.MONGO_URL_DEV as string,
+  mongoUrlTest: process.env.MONGO_URL_TEST as string,
 };
 
 export default config;
