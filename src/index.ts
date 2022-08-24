30b979582e58a008e5941186fc12bb6dfa785bb9
zidni <zidni.rifan7@gmail.com>
2022-08-24 14:12:34 +0700

    simple chat socket server

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,32 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import Server from './Infrastructures/http/Server';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+import ExpressServer from './Infrastructures/http/Server';
 import container from './Infrastructures/container';
 import db from './Infrastructures/database/mongo/db';
 import config from './Commons/config';
 
-(async () => {
-  const { app } = new Server(container);
-  const port = config.serverPort;
+const { app } = new ExpressServer(container);
+const port = config.serverPort;
 
-  db.on('open', () => {
-    app.listen(port, () => {
-      console.log(`Server running at port ${port}`);
-    });
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: ['http://localhost:3000'] },
+});
+
+db.on('open', () => {});
+
+io.on('connection', (socket) => {
+  socket.on('send-chat', ({ chat, from, to }) => {
+    socket.to(to).emit('receive-chat', { chat, from });
   });
-})();
+  socket.on('join-chat', (myId) => {
+    socket.join(myId);
+  });
+});
+
+httpServer.listen(port);
+console.log(`Server running at port ${port}`);
