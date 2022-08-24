b2e98baef8500d0e4447d2bb4e7a190543b63be1
zidni <zidni.rifan7@gmail.com>
2022-08-24 15:48:27 +0700

    add timestamps ChatModel

--- src/Infrastructures/model/Chat.ts
+++ src/Infrastructures/model/Chat.ts
@@ -1,13 +1,16 @@
 import { model, Schema, Types } from 'mongoose';
 
-const chatSchema = new Schema({
-  from: { type: Types.ObjectId, ref: 'User' },
-  to: { type: Types.ObjectId, ref: 'User' },
-  chat: {
-    type: String,
-    required: true,
+const chatSchema = new Schema(
+  {
+    from: { type: Types.ObjectId, ref: 'User' },
+    to: { type: Types.ObjectId, ref: 'User' },
+    chat: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const ChatModel = model('Chat', chatSchema);
 
