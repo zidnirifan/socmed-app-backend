665234711e6916cd0613474e270cae44f624a4fb
zidni <zidni.rifan7@gmail.com>
2022-08-26 20:50:26 +0700

    fix socket chats

--- src/Interfaces/socket/chats/index.ts
+++ src/Interfaces/socket/chats/index.ts
@@ -3,11 +3,11 @@ import { Socket } from 'socket.io';
 import AddChat from '../../../Applications/use_case/AddChat';
 
 const chatsSocket = (socket: Socket, container: Container) => {
-  socket.on('send-chat', ({ chat, from, to }) => {
+  socket.on('send-chat', ({ chat, time, date, from, to }) => {
     const addChat = container.getInstance(AddChat.name);
     addChat.execute({ chat, from, to });
 
-    socket.to(to).emit('receive-chat', { chat, from });
+    socket.to(to).emit('receive-chat', { chat, time, date, from });
   });
 
   socket.on('join-chat', (myId) => {
