21ae05bd767374567b06351c8dd6115e02fe6104
zidni <zidni.rifan7@gmail.com>
2022-08-26 20:17:04 +0700

    fix ChatGet entity

--- src/Domains/chats/entities/ChatGet.ts
+++ src/Domains/chats/entities/ChatGet.ts
@@ -1,4 +1,4 @@
-export interface IChatGet {
+export interface PayloadChatGet {
   id: string;
   from: string;
   to: string;
@@ -6,19 +6,58 @@ export interface IChatGet {
   createdAt: Date;
 }
 
-class ChatGet {
+export interface IChatGet {
   id: string;
   from: string;
   to: string;
   chat: string;
-  createdAt: Date;
+  date: string;
+  time: string;
+}
 
-  constructor(payload: IChatGet) {
+class ChatGet implements IChatGet {
+  id: string;
+  from: string;
+  to: string;
+  chat: string;
+  date: string;
+  time: string;
+
+  constructor(payload: PayloadChatGet) {
     this.id = payload.id;
     this.from = payload.from;
     this.to = payload.to;
     this.chat = payload.chat;
-    this.createdAt = payload.createdAt;
+    this.date = this.formatDate(payload.createdAt);
+    this.time = new Date(payload.createdAt).toLocaleTimeString('id-ID', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  }
+
+  private formatDate(date: Date) {
+    const secondInMs = 1000;
+
+    const seconds = Math.floor(
+      (new Date().valueOf() - date.valueOf()) / secondInMs
+    );
+
+    const minutesInSeconds = 60;
+    const hourInSeconds = 60 * minutesInSeconds;
+    const dayInSeconds = 24 * hourInSeconds;
+    const weekInSeconds = 7 * dayInSeconds;
+
+    let interval = seconds / dayInSeconds;
+    if (interval <= 1) return 'Today';
+
+    interval = seconds / dayInSeconds;
+    if (interval <= 2) return 'Yesterday';
+
+    interval = seconds / weekInSeconds;
+    if (interval <= 1)
+      return new Date(date).toLocaleDateString('en-ID', { weekday: 'long' });
+
+    return new Date(date).toDateString();
   }
 }
 
